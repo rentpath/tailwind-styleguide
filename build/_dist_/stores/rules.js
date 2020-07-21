5e797276e04e78f88c4c7783990350d447895859
Joseph Walker <jwalker@rentpath.com>
2020-07-21 16:48:00 -0500

    Temp

--- build/_dist_/stores/rules.js
+++
@@ -1,8 +0,0 @@
-import {RuleWalker as RuleWalker2} from "../css/RuleWalker.js";
-import {BackgroundColorCollector} from "../sections/backgroundColor/collector.js";
-import {TextColorCollector} from "../sections/textColor/collector.js";
-import {BoxShadowCollector} from "../sections/boxShadow/collector.js";
-import {BorderRadiusCollector} from "../sections/borderRadius/collector.js";
-import {LineHeightCollector} from "../sections/lineHeight/collector.js";
-import {FontSizeCollector} from "../sections/fontSize/collector.js";
-export const ruleWalker = new RuleWalker2([new BackgroundColorCollector(), new TextColorCollector(), new BoxShadowCollector(), new BorderRadiusCollector(), new LineHeightCollector(), new FontSizeCollector()]);
