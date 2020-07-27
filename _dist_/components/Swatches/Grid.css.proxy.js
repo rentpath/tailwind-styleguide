f99c95f89d7de564164ff7e46637d43e03cde774
Joseph Walker <josephwalker@tred.com>
2020-07-27 13:19:37 -0500

    Cleaning step 1

--- _dist_/components/Swatches/Grid.css.proxy.js
+++
@@ -1,9 +0,0 @@
-
-const code = ".swatches.svelte-wpnnzx{display:grid;grid-template-columns:repeat(auto-fill, minmax(96px, 1fr));gap:24px 24px}";
-
-const styleEl = document.createElement("style");
-const codeEl = document.createTextNode(code);
-styleEl.type = 'text/css';
-
-styleEl.appendChild(codeEl);
-document.head.appendChild(styleEl);
