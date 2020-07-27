bdaaa50ede29873d6d273e22ced89f75948f991f
Joseph Walker <josephwalker@tred.com>
2020-07-27 13:25:12 -0500

    Casing

--- _dist_/components/ClassName.css.proxy.js
+++
@@ -1,9 +0,0 @@
-
-const code = "em.svelte-1mo47sy{font-style:normal;font-weight:600;font-size:0.8rem;color:#999;text-align:center}";
-
-const styleEl = document.createElement("style");
-const codeEl = document.createTextNode(code);
-styleEl.type = 'text/css';
-
-styleEl.appendChild(codeEl);
-document.head.appendChild(styleEl);
--- _dist_/components/Icons/Upload.css.proxy.js
+++
@@ -1,9 +0,0 @@
-
-const code = "svg.svelte-1mcijn5{width:64px;height:64px;stroke:var(--primary-color);fill:var(--primary-color)}";
-
-const styleEl = document.createElement("style");
-const codeEl = document.createTextNode(code);
-styleEl.type = 'text/css';
-
-styleEl.appendChild(codeEl);
-document.head.appendChild(styleEl);
